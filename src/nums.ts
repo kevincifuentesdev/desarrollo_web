8ec7028fa922eafc470fd0ccf764551041d1df5a
kevincifuentesdev <kevin.cifuentes@esumer.edu.co>
2026-08-21 18:33:46 -0500

    feat: numbers exercises.

--- src/nums.ts
+++ src/nums.ts
@@ -24,3 +24,14 @@ let nums_2: number[] = create_n_numbers_array(1500)
 console.log(`Para el array [${nums_1}], la suma total es de: ` + add_numbers_from_list(nums_1));
 console.log(`Para el array 2, la suma total es de: ` + add_numbers_from_list(nums_2));
 
+function get_average(array: number[]): number {
+    if (array.length === 0) {
+        return 0;
+    }
+
+    return add_numbers_from_list(array) / array.length;
+}
+
+console.log(`Para el array [${nums_1}], el promedio es ${get_average(nums_1)}`);
+
+console.log(`Los números mayores a 50 son ${nums_1.filter(number => number > 50)}`);
