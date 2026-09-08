424b01aa82453daaac1403606aff413a8e837ee4
kevincifuentesdev <kevin.cifuentes@esumer.edu.co>
2026-09-08 16:37:43 -0500

    feat: deleted directories.

--- src/students.ts
+++
@@ -1,7 +0,0 @@
-let students_name: string[] = ['Kevin', 'Miguel', 'Juan', 'José', 'Thomas'];
-
-for (let i = 0; i < students_name.length; i++) {
-    console.log(students_name[i]);
-}
-
-console.log('\nHay ' + students_name.length + ' estudiantes en total.')
